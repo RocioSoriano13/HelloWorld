6d020c738caaa44e704f6db3a212f5484b72d44e
RocioSoriano13 <rociosoriano1234@gmail.com>
2023-01-22 18:46:30 +0100

    suma

--- src/helloWorld/Main.java
+++ src/helloWorld/Main.java
@@ -9,9 +9,13 @@ public class Main {
 		
 		int cont = 0;
 		int[] contadores = {1,2,3,4};
-		
+		cont=cont+5;
+		cont+=5;
 		
 		System.out.println("Hello WORLD");
+		System.out.println(cont);
+		
+		
 	}
 
 }
